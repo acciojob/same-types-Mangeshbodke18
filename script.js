9b68f7a3ee582cf086ea0a73b45a9b832552a1e5
Mangesh Bodke <mangeshbodke582@gmail.com>
2025-04-08 21:22:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 function isSameType(value1, value2) {
-    // Convert inputs to actual types
-    if (Number.isNaN(value1) && Number.isNaN(value2)) {
-      return true;
-    }
-  
-    if (typeof value1 === typeof value2) {
-      return true;
-    }
-  
-    return false;
+  const isVal1NaN = typeof value1 === "number" && Number.isNaN(value1);
+  const isVal2NaN = typeof value2 === "number" && Number.isNaN(value2);
+
+  if (isVal1NaN && isVal2NaN) {
+    return true;
   }
-  
-  // Example test:
-  let value1 = prompt("Enter Start of the Range.");
-  let value2 = prompt("Enter End Of the Range.");
-  
-  // Convert to number if possible
-  let parsedValue1 = isNaN(Number(value1)) ? value1 : Number(value1);
-  let parsedValue2 = isNaN(Number(value2)) ? value2 : Number(value2);
-  
-  alert(isSameType(parsedValue1, parsedValue2));
-  
+
+  return typeof value1 === typeof value2;
+}
+
+let value1 = prompt("Enter Start of the Range.");
+let value2 = prompt("Enter End Of the Range.");
+
+// Convert to number if possible
+let parsedValue1 = isNaN(Number(value1)) ? value1 : Number(value1);
+let parsedValue2 = isNaN(Number(value2)) ? value2 : Number(value2);
+
+alert(isSameType(parsedValue1, parsedValue2));
